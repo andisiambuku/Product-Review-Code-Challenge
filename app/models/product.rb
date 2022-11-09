cc124224f47d83636540365d2aa6999d8b2f189d
andisiambuku <roselandambuku@gmail.com>
2022-11-09 23:08:13 +0300

    Assignment complete

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,17 @@
 class Product < ActiveRecord::Base
+    has_many :reviews
+    has_many :users, through :reviews
+
+    def write_review(user,star_rating,comment)
+        Review.create(star_rating:star_rating, comment:comment, user_id:user.id, product_id:self.id)
+    end
+
+    def all_reviews
+        self.reviews.map do |r|
+            r.output_review
+    end
+    def mean_rating
+        self.reviews.average(:star_rating).to_f
+    end
     
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
 class User < ActiveRecord::Base
+    has_many :reviews
+    has_many :products, through :reviews
+
+    def liked_product
+        self.reviews.order(:star_rating).last
+    end
+
+    def delete_reviews(product)
+        self.reviews.where(product_id: product.id).destroy_all
+    end
     
 end
